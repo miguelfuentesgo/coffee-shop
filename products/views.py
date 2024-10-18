d6be9975ddc0ccf37ce526362b5fee25763a1521
Miguel Fuentes <miguel_eduard98@hotmail.com>
2024-10-18 17:40:35 -0500

    first commmit

---
+++ products/views.py
@@ -0,0 +1,19 @@
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import ProductForm
+from .models import Product
+
+
+class ProductFormView(generic.FormView):
+    template_name = "products/add_product.html"
+    form_class = ProductForm
+    success_url = reverse_lazy('add_product')
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+class ProductListView(generic.ListView):
+    model=Product
+    template_name= "products/list_products.html"
+    context_object_name = "products"
